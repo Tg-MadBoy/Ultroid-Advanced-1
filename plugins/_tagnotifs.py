e1c3ff34b5c4525bc4f487520b1b1769b8a0b79b
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-02-23 21:51:48 +0530

    Update _tagnotifs.py

--- plugins/_tagnotifs.py
+++ plugins/_tagnotifs.py
@@ -21,7 +21,7 @@ from . import *
 async def all_messages_catcher(event):
     if udB.get("TAG_LOG") is not None:
         NEEDTOLOG = int(udB.get("TAG_LOG"))
-        await event.forward_to(TAG_LOG)
+        await event.forward_to(NEEDTOLOG)
         ammoca_message = ""
         who_ = await event.client.get_entity(event.sender_id)
         if who_.bot or who_.verified or who_.support:
@@ -29,7 +29,7 @@ async def all_messages_catcher(event):
         who_m = f"[{get_display_name(who_)}](tg://user?id={who_.id})"
         where_ = await event.client.get_entity(event.chat_id)
         where_m = get_display_name(where_)
-        button_text = "📨 Go to Message  "
+        button_text = "📨 Check Message  "
         if isinstance(where_, Channel):
             message_link = f"https://t.me/c/{where_.id}/{event.id}"
             chat_link = f"https://t.me/c/{where_.id}"
@@ -39,7 +39,7 @@ async def all_messages_catcher(event):
         ammoca_message += f"{who_m} tagged you in [{where_m}]({chat_link})"
         try:
             await asst.send_message(
-                entity=TAG_LOG,
+                entity=NEEDTOLOG,
                 message=ammoca_message,
                 link_preview=False,
                 buttons=[[custom.Button.url(button_text, message_link)]],
